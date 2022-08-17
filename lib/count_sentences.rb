c6e25812b7e8f2f5256043fefbed8295ca7e2add
David <davidongaroh@gmail.com>
2022-08-17 19:46:56 +0300

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?('.')
   end
 
   def question?
+    self.end_with?('?')
 
   end
 
   def exclamation?
-
+    self.end_with?('!')
   end
 
   def count_sentences
-
+    # using regex to match given punctions, be keen with spaces
+    self.split(/\.|\?|!/).filter{|string| !string.empty?}.length
   end
 end
